3c5b04ef3044f805912169180f4f5ec7257c34fc
jeissonarcadio778 <jeissonbeltranporras@gmail.com>
2023-02-02 12:22:16 -0500

    funda. finished

--- 00-challenges-py/01-rock-paper-scissors.py
+++ 00-challenges-py/01-rock-paper-scissors.py
@@ -2,32 +2,59 @@ import random
 
 options = ('rock', 'paper','scissors')
 
-user_option = input('rock, paper, scissors: ')
-user_option = user_option.lower()
+computer_points = 0
+user_points = 0
+rounds = 1
 
-if not user_option in options: 
-    print('its not a correct choice')
+while True: 
+    print('*' * 10)
+    print('ROUND', rounds)
+    print('*' * 10)
 
-computer_option = random.choice(options)
+    
+    user_option = input('rock, paper, scissors: ')
+    user_option = user_option.lower()
+    
+    while not user_option in options:
+        user_option = input('Again: rock, paper, scissors: ')
+        user_option = user_option.lower()
+    
+    computer_option = random.choice(options)
 
-print('User option => ', user_option)
-print('Computer option => ', computer_option)
+    print('User option => ', user_option)
+    print('Computer option => ', computer_option)
 
+    if user_option == computer_option: 
+        print('Tid')
+    elif user_option == 'paper':
+        if computer_option == 'scissors':
+            print('Computer point')
+            computer_points += 1
+        else:
+            print('User point')
+            user_points += 1
+    elif user_option == 'rock':
+        if computer_option == 'paper':
+            print('Computer point')
+            computer_points += 1
+        else:
+            print('User point')
+            user_points += 1
+    else: 
+        if computer_option == 'rock':
+            print('Computer point')
+            computer_points += 1
+        else:
+            print('User point')
+            user_points += 1
 
-if user_option == computer_option: 
-    print('Tid')
-elif user_option == 'paper':
-    if computer_option == 'scissors':
-        print('Computer Wins')
-    else:
-        print('User Wins')
-elif user_option == 'rock':
-    if computer_option == 'paper':
-        print('Computer Wins')
-    else:
-        print('User Wins')
-else: 
-    if computer_option == 'rock':
-        print('Computer Wins')
-    else:
-        print('User Wins')
+    rounds += 1   
+    
+    if computer_points == 2:
+        print('Computer wins')
+        break 
+
+    if user_points == 2:
+        print('User wins')
+        break
+
