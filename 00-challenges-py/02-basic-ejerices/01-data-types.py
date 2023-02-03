2d0842c3e170e4fa1ee13849615c42eed2e8d90a
jeissonarcadio778 <jeissonbeltranporras@gmail.com>
2023-02-03 10:10:55 -0500

    challenge: data-types

---
+++ 00-challenges-py/02-basic-ejerices/01-data-types.py
@@ -0,0 +1,36 @@
+
+# operation = ( (3+2)/(2*5))**2
+# print(operation)
+
+# Escribir un programa que pregunte al usuario por el número de horas trabajadas y el coste por hora. Después debe mostrar por pantalla la paga que le corresponde.
+
+# hours = int(input('What time do u spend working today?'))
+# cost = float(input('What is the cost of one hour for your work?'))
+# salary = hours * cost
+# print(salary)
+
+# Escribir un programa que lea un entero positivo, 
+# , introducido por el usuario y después muestre en pantalla la suma de todos los enteros desde 1 hasta 
+# . La suma de los 
+#  primeros enteros positivos puede ser calculada de la siguiente forma:
+
+# last_number = int(input("Introduce the last number for your list: "))
+# sum_numbers = (last_number * (last_number + 1))/2 
+# print(sum_numbers)
+
+# Escribir un programa que pida al usuario dos números enteros y muestre por pantalla la <n> entre <m> da un cociente <c> y un resto <r> donde <n> y <m> son los números introducidos por el usuario, y <c> y <r> son el cociente y el resto de la división entera respectivamente.
+
+
+# weight = float(input('Whats your weight in Kg?'))
+# height = float(input('Whats your height in Mts?'))
+# imc = weight / (height**2)
+# print(f"Your IMC is {round(imc,2)}")
+
+
+# Escribir un programa que pida al usuario dos números enteros y muestre por pantalla la <n> entre <m> da un cociente <c> y un resto <r> donde <n> y <m> son los números introducidos por el usuario, y <c> y <r> son el cociente y el resto de la división entera respectivamente.
+
+# n = int(input('Introduce your first num: '))
+# m = int(input('Introduce your second num: '))
+
+
+# print(f"{n} entre {m} da un cociente {round(n/m, 2)} y un resto {n % m }")
