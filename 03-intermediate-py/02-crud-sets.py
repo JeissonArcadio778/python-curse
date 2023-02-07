f456d15690522609954d91533e876047ef6502e6
jeissonarcadio778 <jeissonbeltranporras@gmail.com>
2023-02-07 08:46:58 -0500

    sets: crud

---
+++ 03-intermediate-py/02-crud-sets.py
@@ -0,0 +1,28 @@
+set_countries = {'col', 'mx', 'bol', 'col'}
+
+
+size = len(set_countries)
+print(size) #3
+
+print('peru' in set_countries) # False
+
+
+# CRUD
+
+# CREATE
+set_countries.add('pe')
+set_countries.add('pe')
+print(set_countries)
+
+# UPDATE
+set_countries.update({'ar', 'ecua', 'pe'})
+print(set_countries)
+
+# DELETE
+set_countries.remove('ar') # If not exists: Error.
+set_countries.discard('arg') # If the element is not a member, do nothing.
+print(set_countries)
+
+set_countries.clear()
+print(len(set_countries)) # 0
+
