42034b6ea6df8ac376a8adbd16cd409db69696c0
jeissonarcadio778 <jeissonbeltranporras@gmail.com>
2023-02-14 08:06:40 -0500

    finshed data stucture-flix vs

--- 03-intermediate-py/06-list-vs-tuples-vs-sets.py
+++ 03-intermediate-py/06-list-vs-tuples-vs-sets.py
@@ -2,9 +2,9 @@
 """
 
 Differences between Lists, Tuples, and Sets
-Data type	Mutable	Ordered	Indexing	Duplicate elements
-List	✓	✓	✓	✓
-Tuple	x	✓	✓	✓
-Set	✓	x	x	x
+Data type	Mutable	Ordered	Indexing Duplicate elements
+    List	✓	    ✓	    ✓	        ✓
+    Tuple	x	    ✓	    ✓	        ✓
+    Set	    ✓	    x	    x	        x
 
 """
