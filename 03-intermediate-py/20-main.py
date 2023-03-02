441269516df08e87a1c38991dc28d1b6e24f982a
jeissonarcadio778 <jeissonbeltranporras@gmail.com>
2023-03-02 10:01:50 -0500

    pkg

---
+++ 03-intermediate-py/20-main.py
@@ -0,0 +1,6 @@
+
+def run(): 
+    print('Hello World for your Shell')
+
+if __name__ == '__main__':
+    run()
---
+++ 03-intermediate-py/pkg/__init__.py
@@ -0,0 +1,7 @@
+# His principal function is iniciatilizar el variables de entorno
+
+print('Se inicio paquete')
+
+URL  = 'platzi.com'
+
+import pkg.mod_1, pkg.mod_2
