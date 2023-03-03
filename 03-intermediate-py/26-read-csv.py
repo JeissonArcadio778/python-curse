6d07a9b939ba87b6b63e0ac725e2e7312d48e4f9
jeissonarcadio778 <jeissonbeltranporras@gmail.com>
2023-03-03 16:10:52 -0500

    charts

--- 03-intermediate-py/26-read-csv.py
+++ 03-intermediate-py/26-read-csv.py
@@ -3,6 +3,25 @@ import csv
 def read_csv(path):
 
     with open(path, 'r') as file_csv: 
+        
         reader = csv.reader(file_csv, delimiter = ',')
+        
+        header = next(reader)
+        
+        data = []
+        
+        for row in reader: 
+            
+            iterable = zip(header, row)
+            
+            country_dict = { key : value for key, value in iterable}
+
+            data.append(country_dict)
+
+            return data
+
+if __name__ == '__main__':
+    data = read_csv('./world_population.csv')
+    print(data[0])
 
 
