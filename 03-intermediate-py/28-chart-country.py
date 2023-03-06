ffd564c2c89d083583d4a46c20467fe014c8937a
jeissonarcadio778 <jeissonbeltranporras@gmail.com>
2023-03-06 08:46:12 -0500

    reto al 8%

--- 03-intermediate-py/28-chart-country.py
+++ 03-intermediate-py/28-chart-country.py
@@ -2,6 +2,18 @@
 
 import csv 
 import re
+import matplotlib.pyplot as plt 
+
+years_list = {
+    "2022" : "2022 Population",
+    "2020" : "2020 Population",
+    "2015" : "2015 Population",
+    "2010" : "2010 Population",
+    "2000" : "2000 Population",
+    "1990" : "1990 Population",
+    "1980" : "1980 Population",
+    "1970" : "1970 Population",
+}
 
 def read_csv(path):
 
@@ -21,27 +33,59 @@ def read_csv(path):
 
             data.append(country_dict)
 
-            return data
+        return data
 
 def get_country(country, data):
     
     result_country = list(filter(lambda item : item['Country/Territory'] == country, data))
-    print(result_country)
     return result_country
 
-def get_population_by_country(country):
+def get_population_by_country(data): 
+    
+    country = input('Seleccione un país: ' ).capitalize()
 
-    # if 'Population' in country:
-    pass 
-        
+    countries = list(filter(lambda item : item['Country/Territory'] == country, data))
+
+    return { key: value for (key, value) in countries[0].items() if key in years_list.values()}
+
+def generate_bar_chart(labels, values): 
+    print('Charts!')
+    fig, ax = plt.subplots()
+    ax.bar(labels, values)
+    plt.show()   
+
+def get_population(data):
+    
+    new_dict = {}
+    
+    country = input('Seleccione un país: ' ).capitalize()
+
+    #Obtengo la lista del pais
+    
+    countries = [x for x in data if x['Country/Territory'] == country]
+    
+    #Al ser una lista de diccionarios tomo su primer y unico valor 
+    
+    for key, value in countries[0].items():
+    
+        #Busco si la llave termina en 'ation' de 'Population'
+    
+        if key[-5:] == 'ation':
+    
+            #De ser así tomo los primeros 4 digitos como clave con su respectivo valor
+            new_dict[key[0:4]] = value
+            
+    return new_dict
+
 
 if __name__ == '__main__':
-    data = read_csv('./world_population.csv')
-    print(data[0])
-    
-    user_country = input('Hello, please send the country: ')
-    
-    country = get_country(user_country, data); 
+    data = read_csv('./world_population.csv')    
+    result = get_population(data)
+    print(result)
+
+    result = get_population_by_country(data) 
+    print(result)
+
 
 
     
