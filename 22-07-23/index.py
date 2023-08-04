58a1594ef92ac393fdde86bd0438b6c92dbe2f62
Jeissonarcadio778 <jeissonbeltranporras@gmail.com>
2023-08-03 20:10:28 -0500

    list comprehensions

--- 22-07-23/index.py
+++ 22-07-23/index.py
@@ -1,84 +1,84 @@
-#01-funds
+# 01-funds
 
-print('Hello World')
+print("Hello World")
 name: str = input("whats your name? ")
-words:str = f'Welcome {name} !!!'
+words: str = f"Welcome {name} !!!"
 print(words)
 
 # Type
 print(type(name))
 
 # Stings: are inmutables
-print(words.count('e')) 
-if words.startswith('Wel'):
-    print('Yes!' * 3)
-print(words.replace('Welcome', 'Hello')) 
+print(words.count("e"))
+if words.startswith("Wel"):
+    print("Yes!" * 3)
+print(words.replace("Welcome", "Hello"))
 
 # Arith
-print(2//3) # Int divition 
+print(2 // 3)  # Int divition
 
-#Floats
-print(1.1 + 2.2) #3.300000...
-print(round( 1.1 + 2.2 ), 1) # 3.1 =~ n < 5
+# Floats
+print(1.1 + 2.2)  # 3.300000...
+print(round(1.1 + 2.2), 1)  # 3.1 =~ n < 5
 
-#Lists
-numbers = [1,2,3,4,5,6,7,8]
-people = ['loquita', 'julio', 'maria']
-print(1 in numbers) # Tue
-print(numbers[-1]) # 8
+# Lists
+numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+people = ["loquita", "julio", "maria"]
+print(1 in numbers)  # Tue
+print(numbers[-1])  # 8
 numbers.append(700)
-numbers.insert(0,'Not reemplace') # Don't delete
-numbers.index(2) # 1
-people.remove('maria')
+numbers.insert(0, "Not reemplace")  # Don't delete
+numbers.index(2)  # 1
+people.remove("maria")
 people.pop()
 people.pop(0)
 numbers.reverse()
 lists = numbers + people
-sorting_letters = ['z', 'e', 'a', 'r']
+sorting_letters = ["z", "e", "a", "r"]
 sorting_letters.sort()
 
-#Tuples: are inmutables
-numbers = (1,2,3,4,5)
-names = ('loquita', 'loquito')
+# Tuples: are inmutables
+numbers = (1, 2, 3, 4, 5)
+names = ("loquita", "loquito")
 """
 index, count, convert tuple() - list()
 """
 
-#Dicts: 
+# Dicts:
 presidentOfUSA = {
-    'name': 'Fank',
-    'last_name': 'Underwood',
-    'age': 55,
-    'skin': 'white',
-    'kills': ['Peter Russi', 'Zoe Bares'],
+    "name": "Fank",
+    "last_name": "Underwood",
+    "age": 55,
+    "skin": "white",
+    "kills": ["Peter Russi", "Zoe Bares"],
 }
-president_last_name = presidentOfUSA.get('last_name')
+president_last_name = presidentOfUSA.get("last_name")
 print(f"Francis {presidentOfUSA['last_name']}")
-print(presidentOfUSA['kills'][1]) # Zoe...
+print(presidentOfUSA["kills"][1])  # Zoe...
 
 # CRUD
-presidentOfUSA['isSingle'] = True
-presidentOfUSA['kills'].append('Remy')
+presidentOfUSA["isSingle"] = True
+presidentOfUSA["kills"].append("Remy")
 
-presidentOfUSA['isSingle'] = False
-presidentOfUSA['age'] += 3
+presidentOfUSA["isSingle"] = False
+presidentOfUSA["age"] += 3
 
-del presidentOfUSA['last_name']
-presidentOfUSA.pop('skin')
+del presidentOfUSA["last_name"]
+presidentOfUSA.pop("skin")
 print(presidentOfUSA)
 
 # Some methos
 print("Keys:::", list(presidentOfUSA.keys()))
 print("Values:::", list(presidentOfUSA.values()))
-print("Items:::", list(presidentOfUSA.items())) # [(),()]
+print("Items:::", list(presidentOfUSA.items()))  # [(),()]
 
-#Loops
+# Loops
 
 counter = 0
 while counter < 20:
     print(f"Counter in: {counter}")
     if counter == 15:
-        print('break, STOP!')
+        print("break, STOP!")
         break
     counter += 1
 
@@ -86,13 +86,13 @@ counter = 0
 while counter < 20:
     counter += 1
     if counter < 15:
-        print('Continue...')
+        print("Continue...")
         continue
     print(f"Counter in: {counter}")
 
 # For, for conditions
 
-for number in range(20): # 0-19
+for number in range(20):  # 0-19
     print(number)
 
 for number in numbers:
@@ -114,32 +114,46 @@ data_harry = [
         "species": "human",
         "gender": "female",
     },
-     {
-    
+    {
         "name": "Harry Potter",
         "species": "human",
-        "gender": "male",     
+        "gender": "male",
     },
 ]
 
 for person in data_harry:
-    print(person['name'])
+    print(person["name"])
 
 # loops anidados
 
 matriz = [
-  [1, 2, 3], 
-#   [4, 5, 6], 
-  [7, 8, 9]
+    [1, 2, 3],
+    #   [4, 5, 6],
+    [7, 8, 9],
 ]
 
-for i in range(3): 
+for i in range(3):
     for j in range(3):
         print(matriz[j][i])
 
-print('----------------')
+print("----------------")
 
 for row in matriz:
     print(row)
     for column in row:
         print(column)
+
+
+# MIDDLE PYTHON
+
+"""
+    They don't have duplicates items
+    Doesn't have order 
+    They can be modify
+"""
+
+set_faras = {["julio"], "maria", "valen", "pulga", "yeye", True, "yeye"}
+
+print(set("yeye"))  # {'y', 'e'}
+print(set(("col", "mx", "per")))
+print(set([1, 1, 1, 2]))  # {1, 2} --> list({1,2})
