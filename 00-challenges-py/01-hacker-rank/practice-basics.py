88180bc8d5383f770a18b04e2bba078d952849dc
Jeissonarcadio778 <jeissonbeltranporras@gmail.com>
2023-08-07 11:59:15 -0500

    format doc

--- 00-challenges-py/01-hacker-rank/practice-basics.py
+++ 00-challenges-py/01-hacker-rank/practice-basics.py
@@ -1,16 +1,22 @@
 # Ejercicio 1: Saludo Personalizado
 # Pide al usuario su nombre y luego salúdalo imprimiendo "¡Hola, [nombre]!".
 
-def sayHello( user_name:str ) -> str:
+
+def sayHello(user_name: str) -> str:
     return f"Hello {user_name}!"
+
+
 result: str = sayHello("Yeye")
 print(result)
 
+
 # Ejercicio 2: Suma de Dos Números
 # Pide al usuario dos números y muestra la suma de ambos.
-def sum_numbers ( num_a: int, num_b: int ) -> int:
+def sum_numbers(num_a: int, num_b: int) -> int:
     return num_a + num_b
-result: int = sum_numbers(1,2)
+
+
+result: int = sum_numbers(1, 2)
 print(result)
 
 # Ejercicio 3: Calculadora de Área de un Círculo
@@ -18,8 +24,10 @@ print(result)
 
 import math
 
-def calculate_circle_are (r:float) -> float:
-    return math.pi * (r ** 2) 
+
+def calculate_circle_are(r: float) -> float:
+    return math.pi * (r**2)
+
 
 result: float = calculate_circle_are(3.6)
 print(result)
@@ -30,13 +38,15 @@ print(result)
 # Ejercicio 5: Número Par o Impar
 # Pide al usuario un número e indica si es par o impar.
 
-def isOdd (number: int) -> bool:
-    if (number == 0):
+
+def isOdd(number: int) -> bool:
+    if number == 0:
         return False
-    elif (number % 2 == 0):
+    elif number % 2 == 0:
         return True
     else:
         return False
+
 
 user_number = int(input("Write a number: "))
 print(f"Number {user_number} is {isOdd(user_number)}")
@@ -44,13 +54,17 @@ print(f"Number {user_number} is {isOdd(user_number)}")
 # Ejercicio 6: Mayor de Tres Números
 # Pide al usuario tres números y encuentra el mayor de los tres.
 
+
 def max_number(numbers: [int]):
     max_number = 0
-    for number in numbers: 
-        if number > max_number: 
+    for number in numbers:
+        if number > max_number:
             max_number = number
     return max_number
+
+
 print(f"Max Number {max_number([10,5,1])}")
+
 
 # Min Number:
 def min_number(numbers: [int]) -> list:
@@ -59,37 +73,45 @@ def min_number(numbers: [int]) -> list:
         if number < min_number:
             min_number = number
     return min_number
+
+
 print(f"Min Number {min_number([10,5,1])}")
 
 # Ejercicio 7: Calificación de Examen
 # Pide al usuario su calificación en un examen (0-100) y asigna una letra correspondiente (A, B, C, etc.) según la calificación.
 
+
 def notes(note: int) -> str:
-    if (note > 80):
+    if note > 80:
         return "E"
-    elif (note < 80 and note > 60):
+    elif note < 80 and note > 60:
         return "S"
-    
+
+
 print(notes(70))
 
 # Ejercicio 8: Lista de Compras
 # Permite al usuario ingresar varios elementos en una lista de compras y luego imprime toda la lista.
 
-def show_items_in_list (items: list):
+
+def show_items_in_list(items: list):
     for item in items:
         print(item)
-show_items_in_list(['Water', 'Eggs', 'Rice'])
+
+
+show_items_in_list(["Water", "Eggs", "Rice"])
 
 # Ejercicio 9: Contador de Vocales y Consonantes
 # Pide al usuario que ingrese una cadena y cuenta el número de vocales y consonantes en ella.
 
 import re
 
-def count_vocals_consonants (word: str) -> int:
+
+def count_vocals_consonants(word: str) -> int:
     word = word.lower()
 
-    patter_vocals = r'[aeiou]'
-    patter_consonants = r'[bcdfghjklmnpqrstvwxyz]'
+    patter_vocals = r"[aeiou]"
+    patter_consonants = r"[bcdfghjklmnpqrstvwxyz]"
 
     count_vocals = len(re.findall(patter_consonants, word))
     count_consonants = len(re.findall(patter_consonants, word))
@@ -97,20 +119,22 @@ def count_vocals_consonants (word: str) -> int:
     return count_vocals, count_consonants
 
 
-number_vocals, number_consonants = count_vocals_consonants('CASA')
+number_vocals, number_consonants = count_vocals_consonants("CASA")
 
 print(f"Vocals {number_vocals}. Consonants {number_consonants}")
 
 # Ejercicio 10: Calculadora de Promedio
 # Pide al usuario que ingrese una serie de números (podría ser calificaciones, por ejemplo) y luego calcula e imprime el promedio.
 
-def averague_notes (notes: list) -> int:
-    sum_notes = 0
-    for note in notes: 
-        sum_notes += note     
-    return sum_notes/len(notes)
 
-print(averague_notes([3,3,3]))
+def averague_notes(notes: list) -> int:
+    sum_notes = 0
+    for note in notes:
+        sum_notes += note
+    return sum_notes / len(notes)
+
+
+print(averague_notes([3, 3, 3]))
 
 # PRACTICTE COMPREHENSIONS:
 
@@ -118,28 +142,28 @@ print(averague_notes([3,3,3]))
 
 # 1. Generar una lista con los cuadrados de los primeros 10 números naturales.
 
-numbers = [number for number in range(1,11)]
+numbers = [number for number in range(1, 11)]
 print(numbers)
-numbers_sqrt = [number ** 2 for number in numbers]
-print(numbers_sqrt) 
+numbers_sqrt = [number**2 for number in numbers]
+print(numbers_sqrt)
 # Better way
-squares = [x** 2 for x in range(1,11)]
+squares = [x**2 for x in range(1, 11)]
 
 # 2. Filtrar los números pares de una lista dada.
 
-numbers = [number for number in range(1,11)]
+numbers = [number for number in range(1, 11)]
 odd_numbers = [number for number in numbers if number % 2 == 0]
 print(odd_numbers)
 
 # 3. Convertir una lista de cadenas a una lista de sus respectivas longitudes.
-string_list = ['Loquita', 'Yeye', 'Maria', 'Julio', 'Valen', 'Pulga']
+string_list = ["Loquita", "Yeye", "Maria", "Julio", "Valen", "Pulga"]
 count_string_list = [len(string) for string in string_list]
 print(count_string_list)
 
 # 4. Obtener una lista de las vocales presentes en una cadena de texto.
-full_name = 'Sara Isabel Gomez Jaramillo'
-vocals = 'aeiou'
-vocals_in_full_name = [ vocal for vocal in full_name.lower() if vocal.lower() in vocals]
+full_name = "Sara Isabel Gomez Jaramillo"
+vocals = "aeiou"
+vocals_in_full_name = [vocal for vocal in full_name.lower() if vocal.lower() in vocals]
 print(vocals_in_full_name)
 
 # 5. Generar una lista de las potencias de 2 hasta el exponente 10.
